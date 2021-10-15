0baa556935ce344caeb2965b0ec6f14565c693b7
Matheus <75762783+matheus-mr94@users.noreply.github.com>
2021-10-15 18:03:42 -0300

    Acessando mais de um produto

---
+++ src/br/com/alura/model/Produto.java
@@ -0,0 +1,28 @@
+package br.com.alura.model;
+
+public class Produto {
+
+	private String nome;
+	private double preco;
+
+	public Produto(String nome, double preco) {
+		this.nome = nome;
+		this.preco = preco;
+				
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	@Override
+	public String toString() {
+		return "Nome: " + nome + "\nPreço R$ " + preco +"\n";
+	}
+	
+	
+}
