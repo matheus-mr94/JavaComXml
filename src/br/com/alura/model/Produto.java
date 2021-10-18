feb34540a29948d0c9f0d9f0dcf2cd56900cab8e
Matheus <75762783+matheus-mr94@users.noreply.github.com>
2021-10-18 14:53:14 -0300

    Trabalhando com eventos e SAX

--- src/br/com/alura/model/Produto.java
+++ src/br/com/alura/model/Produto.java
@@ -11,12 +11,25 @@ public class Produto {
 				
 	}
 
+	public Produto() {
+		
+	}
+
 	public String getNome() {
 		return nome;
 	}
 
 	public double getPreco() {
 		return preco;
+	}
+	
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
 	}
 
 	@Override
