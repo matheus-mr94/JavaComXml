8bb5467685514233dd9985829622b8b204fa733b
Matheus <75762783+matheus-mr94@users.noreply.github.com>
2021-10-19 09:56:32 -0300

    realizando consultas com XPath

--- src/br/com/alura/teste/Sistema.java
+++ src/br/com/alura/teste/Sistema.java
@@ -2,6 +2,10 @@ package br.com.alura.teste;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -17,11 +21,15 @@ public class Sistema {
 		fabrica.setAttribute("http://java.sun.com/xml/jaxp/properties/schemaLanguage", "http://www.w3.org/2001/XMLSchema");
 		DocumentBuilder builder = fabrica.newDocumentBuilder();
 		Document document = builder.parse("src/vendas.xml");
-		
 		Element venda = document.getDocumentElement();
 		String moeda = venda.getAttribute("moeda");
 		System.out.println(moeda);
-		NodeList produtos = document.getElementsByTagName("produto");
+		
+		String exp = "/venda/produtos/produto[contains(nome,'Livro')]";
+		XPath path = XPathFactory.newInstance().newXPath();
+			XPathExpression expression = path.compile(exp);
+		
+		NodeList produtos = (NodeList) expression.evaluate(document,XPathConstants.NODESET);
 		
 		for( int i =0; i< produtos.getLength(); i++) {
 		Element produto = (Element)produtos.item(i);
